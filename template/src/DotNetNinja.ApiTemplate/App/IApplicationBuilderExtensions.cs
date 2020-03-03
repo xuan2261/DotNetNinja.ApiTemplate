c346a297c3ef0070ea5d0bbbea06be09a57b20a6
Larry House <dotnet.ninja@outlook.com>
2020-03-02 23:11:09 -0600

    Added helper method to clean up UseDevelopExceptionPageLogic

---
+++ template/src/DotNetNinja.ApiTemplate/App/IApplicationBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace DotNetNinja.ApiTemplate.App
+{
+    public static class IApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseDeveloperExceptionPageInDevelopment(this IApplicationBuilder app,
+            IWebHostEnvironment env, DeveloperExceptionPageOptions options = null)
+        {
+            if (env.IsDevelopment())
+            {
+                if (options != null)
+                {
+                    return app.UseDeveloperExceptionPage(options);
+                }
+
+                return app.UseDeveloperExceptionPage();
+            }
+
+            return app;
+        }
+    }
+}
